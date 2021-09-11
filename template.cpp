dd99f065d21d3d4a1fc959c63c3c209265428619
Visn0 <anton_chernysh@outlook.es>
2021-09-11 12:56:46 +0200

    xd

---
+++ template.cpp
@@ -0,0 +1,26 @@
+/*
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<int> &A)
+{
+  return
+}
+
+int main()
+{
+  vector<int> array({});
+  for (int v : array)
+  {
+    cout << v << " ";
+  }
+  cout << endl;
+  cout << solution(array) << endl;
+}
